19638342e30c9b39a73228b45053abbdc7291024
Raoul Schwagmeier <raoul.schwagmeier@jamitlabs.com>
2020-04-30 08:36:08 +0200

    WIP: Implement data task

--- Sources/Jetworking/Client.swift
+++ Sources/Jetworking/Client.swift
@@ -4,6 +4,10 @@ public final class Client {
     // MARK: - Properties
     private let clientConfiguration: ClientConfiguration
 
+    private lazy var session: URLSession = .init(
+        configuration: .default
+    )
+
     // MARK: - Initialisation
     init(clientConfiguration: ClientConfiguration) {
         self.clientConfiguration = clientConfiguration
@@ -11,6 +15,29 @@ public final class Client {
 
     // MARK: - Methods
     public func get<ResponseType: Codable>(endpoint: String, _ completion: @escaping (Result<ResponseType, Error>) -> Void) {
+        let dataTask = session.dataTask(with: clientConfiguration.baseURL.appendingPathComponent(endpoint)) { [weak self] data, urlResponse, error in
+            if let error = error { return completion(.failure(error)) }
+
+            guard let urlResponse = urlResponse else { return print("URLResonse is nil") }
+
+            self?.handleURLResponse(urlResponse) { result in
+                switch result {
+                case let .success(header):
+                    guard
+                        let data = data,
+                        let decodedData = try? self?.clientConfiguration.decoder.decode(ResponseType.self, from: data)
+                    else { return print("Couldn't decode data") }
+                    // Evaluate Header fields
+
+                    completion(.success(decodedData))
+
+                case let .failure(error):
+                    break
+                }
+            }
+        }
+
+        dataTask.resume()
     }
 
     public func post<BodyType: Codable, ResponseType: Codable>(endpoint: String, body: BodyType, _ completion: @escaping (Result<ResponseType, Error>) -> Void) {
@@ -27,5 +54,18 @@ public final class Client {
 
     public func delete<ResponseType: Codable>(endpoint: String, parameter: [String: Any] = [:], _ completion: @escaping (Result<ResponseType, Error>) -> Void) {
 
+    }
+
+    private func handleURLResponse(_ response: URLResponse, responseHandler: @escaping (Result<[AnyHashable: Any], Error>) -> Void) {
+        guard let httpURLResponse = response as? HTTPURLResponse else { return print("Couldn't cast to HTTPURLResponse") }
+        let statusCode = HTTPStatusCodeType(statusCode: httpURLResponse.statusCode)
+
+        switch statusCode {
+        case .successful:
+            responseHandler(.success(httpURLResponse.allHeaderFields))
+
+        default:
+            return
+        }
     }
 }
--- Tests/JetworkingTests/JetworkingTests.swift
+++ Tests/JetworkingTests/JetworkingTests.swift
@@ -2,19 +2,29 @@ import XCTest
 @testable import Jetworking
 
 final class JetworkingTests: XCTestCase {
+    private struct GetResult: Codable {
+        let url: String
+    }
+
     func testExample() {
-        let configuration = ClientConfiguration(baseURL: URL(string: "://")!, encoder: JSONEncoder(), decoder: JSONDecoder())
+        let configuration = ClientConfiguration(baseURL: URL(string: "https://postman-echo.com")!, encoder: JSONEncoder(), decoder: JSONDecoder())
         let client = Client(clientConfiguration: configuration)
 
-        client.get(endpoint: "") { (result: Result<String, Error>) in
+        let expectation = self.expectation(description: "Wait for get")
+
+        client.get(endpoint: "get") { (result: Result<GetResult, Error>) in
             switch result {
             case .failure:
                 break
 
             case let .success(resultData):
-                print(resultData)
+                print(resultData.url)
             }
+
+            expectation.fulfill()
         }
+
+        waitForExpectations(timeout: 5.0, handler: nil) 
     }
 
     static var allTests = [
