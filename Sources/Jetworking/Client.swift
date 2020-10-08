3cfe378034c403bc1a53655097666e8ebd798fa4
Simon Blum <Simon.Blum@jamitlabs.com>
2020-10-08 15:36:06 +0200

    Add implementations for other http methods

--- Sources/Jetworking/Client.swift
+++ Sources/Jetworking/Client.swift
@@ -43,19 +43,125 @@ public final class Client {
     }
 
     public func post<BodyType: Codable, ResponseType: Codable>(endpoint: String, body: BodyType, _ completion: @escaping (Result<ResponseType, Error>) -> Void) {
+        var request: URLRequest = .init(url: clientConfiguration.baseURL.appendingPathComponent(endpoint))
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
 
+        let bodyData: Data = try! clientConfiguration.encoder.encode(body)
+        let dataTask = session.uploadTask(with: request, from: bodyData) { [weak self] data, urlResponse, error in
+            if let error = error { return completion(.failure(error)) }
+
+            guard let urlResponse = urlResponse else { return print("URLResonse is nil") }
+
+            self?.handleURLResponse(urlResponse) { result in
+                switch result {
+                case let .success(header):
+                    guard
+                        let data = data,
+                        let decodedData = try? self?.clientConfiguration.decoder.decode(ResponseType.self, from: data)
+                    else { return print("Couldn't decode data") }
+                    // Evaluate Header fields
+
+                    completion(.success(decodedData))
+
+                case let .failure(error):
+                    break
+                }
+            }
+        }
+
+        dataTask.resume()
     }
 
     public func put<BodyType: Codable, ResponseType: Codable>(endpoint: String, body: BodyType, _ completion: @escaping (Result<ResponseType, Error>) -> Void) {
+        var request: URLRequest = .init(url: clientConfiguration.baseURL.appendingPathComponent(endpoint))
+        request.httpMethod = "PUT"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
 
+        let bodyData: Data = try! clientConfiguration.encoder.encode(body)
+        let dataTask = session.uploadTask(with: request, from: bodyData) { [weak self] data, urlResponse, error in
+            if let error = error { return completion(.failure(error)) }
+
+            guard let urlResponse = urlResponse else { return print("URLResonse is nil") }
+
+            self?.handleURLResponse(urlResponse) { result in
+                switch result {
+                case let .success(header):
+                    guard
+                        let data = data,
+                        let decodedData = try? self?.clientConfiguration.decoder.decode(ResponseType.self, from: data)
+                    else { return print("Couldn't decode data") }
+                    // Evaluate Header fields
+
+                    completion(.success(decodedData))
+
+                case let .failure(error):
+                    break
+                }
+            }
+        }
+
+        dataTask.resume()
     }
 
     public func patch<BodyType: Codable, ResponseType: Codable>(endpoint: String, body: BodyType, _ completion: @escaping (Result<ResponseType, Error>) -> Void) {
+        var request: URLRequest = .init(url: clientConfiguration.baseURL.appendingPathComponent(endpoint))
+        request.httpMethod = "PATCH"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
 
+        let bodyData: Data = try! clientConfiguration.encoder.encode(body)
+        let dataTask = session.uploadTask(with: request, from: bodyData) { [weak self] data, urlResponse, error in
+            if let error = error { return completion(.failure(error)) }
+
+            guard let urlResponse = urlResponse else { return print("URLResonse is nil") }
+
+            self?.handleURLResponse(urlResponse) { result in
+                switch result {
+                case let .success(header):
+                    guard
+                        let data = data,
+                        let decodedData = try? self?.clientConfiguration.decoder.decode(ResponseType.self, from: data)
+                    else { return print("Couldn't decode data") }
+                    // Evaluate Header fields
+
+                    completion(.success(decodedData))
+
+                case let .failure(error):
+                    break
+                }
+            }
+        }
+
+        dataTask.resume()
     }
 
     public func delete<ResponseType: Codable>(endpoint: String, parameter: [String: Any] = [:], _ completion: @escaping (Result<ResponseType, Error>) -> Void) {
+        var request: URLRequest = .init(url: clientConfiguration.baseURL.appendingPathComponent(endpoint))
+        request.httpMethod = "DELETE"
 
+        let dataTask = session.dataTask(with: request) { [weak self] data, urlResponse, error in
+            if let error = error { return completion(.failure(error)) }
+
+            guard let urlResponse = urlResponse else { return print("URLResonse is nil") }
+
+            self?.handleURLResponse(urlResponse) { result in
+                switch result {
+                case let .success(header):
+                    guard
+                        let data = data,
+                        let decodedData = try? self?.clientConfiguration.decoder.decode(ResponseType.self, from: data)
+                    else { return print("Couldn't decode data") }
+                    // Evaluate Header fields
+
+                    completion(.success(decodedData))
+
+                case let .failure(error):
+                    break
+                }
+            }
+        }
+
+        dataTask.resume()
     }
 
     private func handleURLResponse(_ response: URLResponse, responseHandler: @escaping (Result<[AnyHashable: Any], Error>) -> Void) {
--- Tests/JetworkingTests/JetworkingTests.swift
+++ Tests/JetworkingTests/JetworkingTests.swift
@@ -6,8 +6,15 @@ final class JetworkingTests: XCTestCase {
         let url: String
     }
 
-    func testExample() {
-        let configuration = ClientConfiguration(
+    private struct Body: Codable {
+        let foo1: String
+        let foo2: String
+    }
+
+    private struct VoidResult: Codable {}
+
+    func testGetRequest() {
+		let configuration = ClientConfiguration(
             baseURL: URL(string: "https://postman-echo.com")!,
             authenticationMethod: .basicAuthentication(username: "username", password: "password"),
             encoder: JSONEncoder(),
@@ -32,7 +39,98 @@ final class JetworkingTests: XCTestCase {
         waitForExpectations(timeout: 5.0, handler: nil) 
     }
 
+    func testPostRequest() {
+        let configuration = ClientConfiguration(baseURL: URL(string: "https://postman-echo.com")!, authenticationMethod: .none, encoder: JSONEncoder(), decoder: JSONDecoder())
+        let client = Client(clientConfiguration: configuration)
+
+        let expectation = self.expectation(description: "Wait for post")
+
+        let body: Body = .init(foo1: "bar1", foo2: "bar2")
+        client.post(endpoint: "post", body: body) { (result: Result<VoidResult, Error>) in
+            switch result {
+            case .failure:
+                break
+
+            case let .success(resultData):
+                print(resultData)
+            }
+
+            expectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 5.0, handler: nil)
+    }
+
+    func testPutRequest() {
+        let configuration = ClientConfiguration(baseURL: URL(string: "https://postman-echo.com")!, authenticationMethod: .none, encoder: JSONEncoder(), decoder: JSONDecoder())
+        let client = Client(clientConfiguration: configuration)
+
+        let expectation = self.expectation(description: "Wait for post")
+
+        let body: Body = .init(foo1: "bar1", foo2: "bar2")
+        client.put(endpoint: "put", body: body) { (result: Result<VoidResult, Error>) in
+            switch result {
+            case .failure:
+                break
+
+            case let .success(resultData):
+                print(resultData)
+            }
+
+            expectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 5.0, handler: nil)
+    }
+
+    func testPatchRequest() {
+        let configuration = ClientConfiguration(baseURL: URL(string: "https://postman-echo.com")!, authenticationMethod: .none, encoder: JSONEncoder(), decoder: JSONDecoder())
+        let client = Client(clientConfiguration: configuration)
+
+        let expectation = self.expectation(description: "Wait for post")
+
+        let body: Body = .init(foo1: "bar1", foo2: "bar2")
+        client.patch(endpoint: "patch", body: body) { (result: Result<VoidResult, Error>) in
+            switch result {
+            case .failure:
+                break
+
+            case let .success(resultData):
+                print(resultData)
+            }
+
+            expectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 5.0, handler: nil)
+    }
+    
+    func testDeleteRequest() {
+        let configuration = ClientConfiguration(baseURL: URL(string: "https://postman-echo.com")!, authenticationMethod: .none, encoder: JSONEncoder(), decoder: JSONDecoder())
+        let client = Client(clientConfiguration: configuration)
+
+        let expectation = self.expectation(description: "Wait for post")
+
+        client.delete(endpoint: "delete") { (result: Result<VoidResult, Error>) in
+            switch result {
+            case .failure:
+                break
+
+            case let .success(resultData):
+                print(resultData)
+            }
+
+            expectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 5.0, handler: nil)
+    }
+
     static var allTests = [
-        ("testExample", testExample),
+        ("testGetRequest", testGetRequest),
+        ("testPostRequest", testPostRequest),
+        ("testPutRequest", testPutRequest),
+        ("testPatchRequest", testPatchRequest),
+        ("testDeleteRequest", testDeleteRequest)
     ]
 }
