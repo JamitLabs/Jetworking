06afe7eb0cb713e969ad8695d13578fd346502fb
Simon Blum <Simon.Blum@jamitlabs.com>
2020-10-21 11:01:33 +0200

    Create separate initialiser for the background URLSessionConfiguration to pass the other session configuration to

--- Sources/Jetworking/Client/Executor/DownloadExecutor/Background/BackgroundDownloadExecutor.swift
+++ Sources/Jetworking/Client/Executor/DownloadExecutor/Background/BackgroundDownloadExecutor.swift
@@ -7,38 +7,11 @@ final class BackgroundDownloadExecutor: NSObject, DownloadExecutor {
     private var backgroundIdentifier: String
     private var isDiscretionary: Bool = false
     private lazy var session: URLSession = {
-        let configuration: URLSessionConfiguration = .background(withIdentifier: backgroundIdentifier)
-        configuration.isDiscretionary = isDiscretionary
-
-        
-        if #available(iOS 11.0, OSX 10.13, *) {
-            configuration.waitsForConnectivity = sessionConfiguration.waitsForConnectivity
-        }
-
-        if #available(iOS 13.0, OSX 10.15, *) {
-            configuration.allowsConstrainedNetworkAccess = sessionConfiguration.allowsConstrainedNetworkAccess
-            configuration.allowsExpensiveNetworkAccess = sessionConfiguration.allowsExpensiveNetworkAccess
-            configuration.tlsMinimumSupportedProtocolVersion = sessionConfiguration.tlsMinimumSupportedProtocolVersion
-            configuration.tlsMaximumSupportedProtocolVersion = sessionConfiguration.tlsMaximumSupportedProtocolVersion
-        }
-        
-        configuration.allowsCellularAccess = sessionConfiguration.allowsCellularAccess
-        configuration.requestCachePolicy = sessionConfiguration.requestCachePolicy
-        configuration.timeoutIntervalForRequest = sessionConfiguration.timeoutIntervalForRequest
-        configuration.timeoutIntervalForResource = sessionConfiguration.timeoutIntervalForResource
-        configuration.networkServiceType = sessionConfiguration.networkServiceType
-        configuration.connectionProxyDictionary = sessionConfiguration.connectionProxyDictionary
-        configuration.httpShouldUsePipelining = sessionConfiguration.httpShouldUsePipelining
-        configuration.httpShouldSetCookies = sessionConfiguration.httpShouldSetCookies
-        configuration.httpCookieAcceptPolicy = sessionConfiguration.httpCookieAcceptPolicy
-        configuration.httpAdditionalHeaders = sessionConfiguration.httpAdditionalHeaders
-        configuration.httpMaximumConnectionsPerHost = sessionConfiguration.httpMaximumConnectionsPerHost
-        configuration.httpCookieStorage = sessionConfiguration.httpCookieStorage
-        configuration.urlCredentialStorage = sessionConfiguration.urlCredentialStorage
-        configuration.urlCache = sessionConfiguration.urlCache
-        configuration.shouldUseExtendedBackgroundIdleMode = sessionConfiguration.shouldUseExtendedBackgroundIdleMode
-        configuration.protocolClasses = sessionConfiguration.protocolClasses
-
+        let configuration: URLSessionConfiguration = .background(
+            withIdentifier: backgroundIdentifier,
+            andIsDiscretionaryFlag: isDiscretionary,
+            andConfiguration: sessionConfiguration
+        )
         let session: URLSession = .init(configuration: configuration, delegate: self, delegateQueue: nil)
         return session
     }()
