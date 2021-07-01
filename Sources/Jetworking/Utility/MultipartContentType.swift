ad3593612c5d2f54a7a2bd64359976554ee7e1a6
Murat Yilmaz <murat.yilmaz@jamitlabs.com>
2021-07-01 13:32:16 +0200

    Add extension support to multipart type

--- Sources/Jetworking/Utility/MultipartContentType.swift
+++ Sources/Jetworking/Utility/MultipartContentType.swift
@@ -1,37 +1,41 @@
 import Foundation
 
-public enum MultipartContentType: String {
-    case applicationOctetStream = "application/octet-stream"
-    case applicationPDF = "application/pdf"
-    case applicationPKCS8 = "application/pkcs8"
-    case applicationPKCS10 = "application/pkcs10"
-    case applicationPKCS12 = "application/pkcs12"
-    case applicationZip = "application/zip"
-    case applicationGzip = "application/gzip"
-    case applicationJSON = "application/json"
-    case applicationJWT = "application/jwt"
-    case applicationMP4 = "application/mp4"
-    case applicationRTF = "application/rtf"
-    case applicationVCardJSON = "application/vcard+json"
-    case applicationVCardXML = "application/vcard+xml"
-    case applicationXML = "application/xml"
+public struct MultipartContentType: RawRepresentable {
+    public let rawValue: String
 
-    case audioAAC = "audio/aac"
-    case audioMPEG = "audio/mpeg"
-    case audioVorbis = "audio/vorbis"
-    
-    case imageJPEG = "image/jpeg"
-    case imagePNG = "image/png"
-    case imageSVG = "image/svg+xml"
-    case imageHEIC = "image/heic"
-    case imageTIFF = "image/tiff"
+    public init(rawValue: String) {
+        self.rawValue = rawValue
+    }
+}
 
-    case textPlain = "text/plain"
-    case textCSV = "text/csv"
-    case textHTML = "text/html"
-    case textMarkdown = "text/markdown"
-    case textRTF = "text/rtf"
-    case textXML = "text/xml"
-
-    case videoMP4 = "video/mp4"
+public extension MultipartContentType {
+    static let applicationOctetStream: Self = .init(rawValue: "application/octet-stream")
+    static let applicationPDF: Self = .init(rawValue: "application/pdf")
+    static let applicationPKCS8: Self = .init(rawValue: "application/pkcs8")
+    static let applicationPKCS10: Self = .init(rawValue: "application/pkcs10")
+    static let applicationPKCS12: Self = .init(rawValue: "application/pkcs12")
+    static let applicationZip: Self = .init(rawValue: "application/zip")
+    static let applicationGzip: Self = .init(rawValue: "application/gzip")
+    static let applicationJSON: Self = .init(rawValue: "application/json")
+    static let applicationJWT: Self = .init(rawValue: "application/jwt")
+    static let applicationMP4: Self = .init(rawValue: "application/mp4")
+    static let applicationRTF: Self = .init(rawValue: "application/rtf")
+    static let applicationVCardJSON: Self = .init(rawValue: "application/vcard+json")
+    static let applicationVCardXML: Self = .init(rawValue: "application/vcard+xml")
+    static let applicationXML: Self = .init(rawValue: "application/xml")
+    static let audioAAC: Self = .init(rawValue: "audio/aac")
+    static let audioMPEG: Self = .init(rawValue: "audio/mpeg")
+    static let audioVorbis: Self = .init(rawValue: "audio/vorbis")
+    static let imageJPEG: Self = .init(rawValue: "image/jpeg")
+    static let imagePNG: Self = .init(rawValue: "image/png")
+    static let imageSVG: Self = .init(rawValue: "image/svg+xml")
+    static let imageHEIC: Self = .init(rawValue: "image/heic")
+    static let imageTIFF: Self = .init(rawValue: "image/tiff")
+    static let textPlain: Self = .init(rawValue: "text/plain")
+    static let textCSV: Self = .init(rawValue: "text/csv")
+    static let textHTML: Self = .init(rawValue: "text/html")
+    static let textMarkdown: Self = .init(rawValue: "text/markdown")
+    static let textRTF: Self = .init(rawValue: "text/rtf")
+    static let textXML: Self = .init(rawValue: "text/xml")
+    static let videoMP4: Self = .init(rawValue: "video/mp4")
 }
--- Sources/Jetworking/Utility/MultipartType.swift
+++ Sources/Jetworking/Utility/MultipartType.swift
@@ -1,13 +1,21 @@
 import Foundation
 
-public enum MultipartType: String {
-    case formData = "multipart/form-data"
-    case mixed = "multipart/mixed"
-    case byteRanges = "multipart/byteranges"
-    case encrypted = "multipart/encrypted"
-    case headerSet = "multipart/header-set"
-    case multilingual = "multipart/multilingual"
-    case related = "multipart/related"
-    case report = "multipart/report"
-    case signed = "multipart/signed"
+public struct MultipartType: RawRepresentable {
+    public let rawValue: String
+
+    public init(rawValue: String) {
+        self.rawValue = rawValue
+    }
+}
+
+public extension MultipartType {
+    static let formData: Self = .init(rawValue: "multipart/form-data")
+    static let mixed: Self = .init(rawValue: "multipart/mixed")
+    static let byteRanges: Self = .init(rawValue: "multipart/byteranges")
+    static let encrypted: Self = .init(rawValue: "multipart/encrypted")
+    static let headerSet: Self = .init(rawValue: "multipart/header-set")
+    static let multilingual: Self = .init(rawValue: "multipart/multilingual")
+    static let related: Self = .init(rawValue: "multipart/related")
+    static let report: Self = .init(rawValue: "multipart/report")
+    static let signed: Self = .init(rawValue: "multipart/signed")
 }
