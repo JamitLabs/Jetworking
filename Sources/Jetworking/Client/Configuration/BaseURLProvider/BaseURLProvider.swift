5271168eb4dc38720f0349c2c658ab6abda07fda
Frederick Pietschmann <19194800+fredpi@users.noreply.github.com>
2022-07-14 19:21:33 +0200

    Add documentation for some more types

--- Sources/Jetworking/Client/Configuration/BaseURLProvider/BaseURLProvider.swift
+++ Sources/Jetworking/Client/Configuration/BaseURLProvider/BaseURLProvider.swift
@@ -1,5 +1,8 @@
 import Foundation
 
+/// A protocol providing a `baseURL` of type `URL`.
+///
+/// Note: `URL` itself is implementing the protocol itself, returning `self` as the `baseURL`.
 public protocol BaseURLProvider {
     var baseURL: URL { get }
 }
